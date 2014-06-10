104e72f618963e7d3f94130607e3cc73914fba28
Laura DeGroot <laurardegroot@gmail.com>
2014-06-10 12:14:40 -0400

    update to newest version of woop

--- js/script.js
+++ js/script.js
@@ -2,35 +2,26 @@ $(function(){
 	useQuerySrc();	
 });
 
-function useQuerySrc(){
-	var imageSrc = getUrlVars()["src"];
-	if (undefined !== imageSrc && imageSrc.length){
-		$('.image img:first-child').replaceWith("<img src='"+imageSrc+"'/>");
-		woop();
-	} else{
-		woop();
-	}
-}
+var source = $('.source');
 
 function woop(){	
-	var levels = getUrlVars()["levels"];
+	var levels = getUrlVars()["levels"]; // are number of levels specified?
 	for (var i=0; i < (levels || 15 ) ; i++){
-		var image = $('.image img:last-child');
-		var newImage = image.clone().appendTo('.image');
-		var prevImage = newImage.prev();
-		var prevWidth = prevImage.width();
-		var newWidth = (prevWidth / 120) * 100;
-		newImage.css('width', newWidth);
+		var lastSource = $('.sources .source:last-child'); 
+		var newSource = lastSource.clone().appendTo('.sources'); // duplicate last media source and append to images
+		var prevSource = newSource.prev();
+		var prevWidth = prevSource.width(); // get width of media source above new media source
+		var newWidth = (prevWidth / 120) * 100; 
+		newSource.css('width', newWidth); // set width of new media source
 	}
 	setShape();
 	setAnimate();
 }
 
-function getUrlVars(){
+function getUrlVars(){ // get query string parameters
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
+    for(var i = 0; i < hashes.length; i++){
         hash = hashes[i].split('=');
         vars.push(hash[0]);
         vars[hash[0]] = hash[1];
@@ -38,22 +29,60 @@ function getUrlVars(){
     return vars;    
 }
 
+function useQuerySrc(){
+	var videoSrc = getUrlVars()["video"];
+	var imageSrc = getUrlVars()["src"]; // is an alternate image src specified?
+	if (undefined !== imageSrc && imageSrc.length){
+		$('.source:first-child').replaceWith("<img src='"+imageSrc+"' class='source'/>");
+		woop();
+	} else if ( videoSrc === "yes") {
+		$('.source:first-child').replaceWith("<video autoplay class='source'></video>");
+		getVideo();
+	}
+	else{
+		woop();
+	}
+}
+
 function setShape(){
-	var shape = getUrlVars()["shape"];
+	var shape = getUrlVars()["shape"]; // is a shape specified?
 	if ( shape === "square" ) {
-		$('.image img:first-child').siblings().css('border-radius','none');
+		$('.source:first-child').siblings().css('border-radius','none');
 	} else {
-		$('.image img:first-child').siblings().css('border-radius','100%');
+		$('.source:first-child').siblings().css('border-radius','100%');
 	}
 }
 
 function setAnimate(){
-	var animate = getUrlVars()["animate"];
+	var animate = getUrlVars()["animate"]; // are we animating this baby?
 	if ( animate === "yes" ) {
-		$('.image img:nth-child(odd)').addClass('animate-odd');
-		$('.image img:nth-child(even)').addClass('animate-even');
-		$('.image img:first-child').removeClass();
+		$('.sources .source:nth-child(odd)').addClass('animate-odd'); // odd numbered media sources go clockwise
+		$('.sources .source:nth-child(even)').addClass('animate-even'); // even numbered media sources go counter clockwise
+		$('.source:first-child').removeClass(); // first media source stays put
 	} else {
-		$('.image img:nth-child(even)').siblings().removeClass();
+		$(source).removeClass('animate-odd');
+		$(source).removeClass('animate-even');
+	}
+}
+
+function getVideo(){
+ 	var errorCallback = function(e) {  
+	   alert('awww, do it', e); // alert when user does not allow cam
+	 };
+
+	navigator.getUserMedia  = navigator.getUserMedia ||
+	                          navigator.webkitGetUserMedia ||
+	                          navigator.mozGetUserMedia ||
+	                          navigator.msGetUserMedia;
+
+	var video = document.querySelector('video');
+
+	if (navigator.getUserMedia) {
+	  navigator.getUserMedia({audio: true, video: true}, function(stream) {
+	    video.src = window.URL.createObjectURL(stream);
+	    woop();
+	  }, errorCallback);
+	} else {
+	  alert("you can't webrtc with your browser") // alert when user's browser doesnt support webrtc
 	}
 }
